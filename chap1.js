c4d1428625c5f1042637ec9cdceea39be79196eb
Andre Ward <andre.ward62@gmail.com>
2020-06-16 20:22:37 -0300

    exs 1 and 2

--- chap1.js
+++ chap1.js
@@ -8,8 +8,9 @@
 // ######
 // #######
 
-
-
+for (let i = "#"; i.length <= 7; i+="#") {
+    console.log(i)
+  }
 
 
 
@@ -18,10 +19,17 @@
 //For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 // When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
-
-
-
-
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }  
+  }
 
 
 
